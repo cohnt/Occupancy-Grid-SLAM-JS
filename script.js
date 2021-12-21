543214db114d4222b1e82dd136f9e5f5d613a9bf
Thomas Cohn <cohnt@umich.edu>
2021-12-21 11:41:14 -0500

    Some clicking-based controls.

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ var worldScale = 20; //The width of the entire browser viewport (in meters), det
 /// GLOBAL VARIABLES ///
 ////////////////////////
 
-var worldCanvas;
-var mapCanvas;
+var worldCanvas; //HTML element object of the world canvas
+var mapCanvas; //HTML element object of the map canvas
+var worldCtx; //Canvas drawing context of the world canvas
+var mapCtx; //Canvas drawing context of the map context
 
 ///////////////
 /// CLASSES ///
@@ -30,6 +32,28 @@ function setup() {
 	worldCanvas.setAttribute("height", String(window.innerHeight * canvasSize[1]) + "px");
 	mapCanvas.setAttribute("width", String(window.innerWidth * canvasSize[0]) + "px");
 	mapCanvas.setAttribute("height", String(window.innerHeight * canvasSize[1]) + "px");
+
+	// Create event listeners for the various controls on the page
+	document.getElementById("startButton").addEventListener("click", startButtonClick);
+	document.getElementById("pauseButton").addEventListener("click", pauseButtonClick);
+	document.getElementById("resetButton").addEventListener("click", resetButtonClick);
+	document.getElementById("newWorldButton").addEventListener("click", newWorldButtonClick);
+}
+
+function startButtonClick() {
+	//
+}
+
+function pauseButtonClick() {
+	//
+}
+
+function resetButtonClick() {
+	//
+}
+
+function newWorldButtonClick() {
+	//
 }
 
 /////////////////////
