ff1cf9acb4ea108fa81f9e2e3409a1e6fc979f4e
Thomas Cohn <cohnt@umich.edu>
2021-12-21 11:29:23 -0500

    Properly size the canvas on page load.

--- simulation.js
+++ simulation.js
@@ -2,13 +2,14 @@
 /// CONSTANTS ///
 /////////////////
 
-//
+var canvasSize = [0.49, 0.7]; //Size of each canvas, as a fraction of the total page width and height, respectively
 
 ////////////////////////
 /// GLOBAL VARIABLES ///
 ////////////////////////
 
-//
+var worldCanvas;
+var mapCanvas;
 
 ///////////////
 /// CLASSES ///
@@ -20,10 +21,18 @@
 /// FUNCTIONS ///
 /////////////////
 
-//
+function setup() {
+	worldCanvas = document.getElementById("worldCanvas");
+	mapCanvas = document.getElementById("mapCanvas");
+
+	worldCanvas.setAttribute("width", String(window.innerWidth * canvasSize[0]) + "px");
+	worldCanvas.setAttribute("height", String(window.innerHeight * canvasSize[1]) + "px");
+	mapCanvas.setAttribute("width", String(window.innerWidth * canvasSize[0]) + "px");
+	mapCanvas.setAttribute("height", String(window.innerHeight * canvasSize[1]) + "px");
+}
 
 /////////////////////
 /// EXECUTED CODE ///
 /////////////////////
 
-//
+requestAnimationFrame(setup);
